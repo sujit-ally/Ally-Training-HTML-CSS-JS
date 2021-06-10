15ba419c573a1929614aff92b9df4a017754cd56
Sujit26 <2017csb1115@iitrpr.ac.in>
2021-06-10 23:14:07 +0530

    add ruby files

--- Training/HTML CSS JS/Ruby/first_file.rb
+++ Training/HTML CSS JS/Ruby/first_file.rb
@@ -1,1 +1,38 @@
+class Book
+  def initialize(bookName, author, price)
+    @book_name =bookName
+    @author =author
+    @price =price
+  end
 
+  def get_details
+    puts "Book Name is :" + @book_name
+    puts "Author Name is :" + @author
+    puts "Price is :" + @price.to_s
+  end
+end
+
+new_book = Book.new("XYZ", "Sachin", 1000)
+new_book.get_details
+
+#c) Score = 100(if else)
+#           Check the score is greater than 50 – then half century
+#           Check the score is greater than 100 – then century using comparison operators
+#
+#
+# b)word = "Determination"
+#
+# check how many characters word have?
+#
+# word is having "tion"?
+# word is having "tic
+# clear the word..check is that empty?
+#  5)Use IRB to work out few numeric methods
+# age=27
+#
+#
+# 1. My age : 27
+# 2. Next year my age would be 28.
+# 3. Verify the age is in between 20 to 30
+# 4. Verify the age is exactly 27 or not
+#            5. Print 5 times age with the help of "times" methods
--- Training/HTML CSS JS/Ruby/q2.rb
+++ Training/HTML CSS JS/Ruby/q2.rb
@@ -1,1 +1,16 @@
+class Book
+  def initialize(bookName, author, price)
+    @book_name =bookName
+    @author =author
+    @price =price
+  end
 
+  def get_details
+    puts "Book Name is :" + @book_name
+    puts "Author Name is :" + @author
+    puts "Price is :" + @price
+  end
+end
+
+new_book = Book.new("XYZ", "Sachin", 1000)
+new_book.get_details()
