2ec9066e9bb3a587590442489f26096787491426
Sujit26 <2017csb1115@iitrpr.ac.in>
2021-06-15 21:04:57 +0530

    upload quiz app

---
+++ Training/HTML CSS JS/JS Assignment/Quiz-App/data.js
@@ -0,0 +1,27 @@
+data = [
+    {
+        question: "How are you?",
+        a: "I'm good",
+        b: "I'm fine",
+        c: "Good, you tell",
+        d: "Not well",
+        answer: "c",
+    },
+    {
+        question: "What is your name?",
+        a: "Sujit Jaiwaliya",
+        b: "Sidhant Jain",
+        c: "Pankaj",
+        d: "Khare",
+        answer: "c",
+    },
+    {
+        question: "How are you?",
+        a: "I'm good",
+        b: "I'm fine",
+        c: "Good, you tell",
+        d: "Not well",
+        answer: "c",
+    },
+
+]
